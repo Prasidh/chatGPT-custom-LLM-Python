42e71b9327be43a3f1688c1e33d047b1c3250df5
Prasidh Sriram <sid@fitmatch.ai>
2023-05-06 14:00:21 -0400

    Added files

---
+++ interface.py
@@ -0,0 +1,18 @@
+from llama_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
+import gradio as gr
+import os
+
+os.environ["OPENAI_API_KEY"] = ''
+
+def chatbot(input_text):
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = index.query(input_text, response_mode="compact")
+    return response.response
+
+iface = gr.Interface(fn=chatbot,
+                     inputs=gr.components.Textbox(lines=7, label="Enter your text"),
+                     outputs="text",
+                     title="Custom-trained AI Chatbot")
+
+iface.launch(share=True)
---
+++ train.py
@@ -0,0 +1,25 @@
+import os
+from llama_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = ''
+
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index.save_to_disk('index.json')
+
+    return index
+
+_index = construct_index("docs")
